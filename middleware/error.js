81cc0033decd0ac6c161d50f55474d4ec8b0fff3
Dheerenda Singh <dsingh110096@gmail.com>
2020-07-23 21:23:02 +0530

    async middleware for removing trycatch block

--- middleware/error.js
+++ middleware/error.js
@@ -6,12 +6,24 @@ const errorHandler = (err, req, res, next) => {
   error.message = err.message;
 
   //Loging to the console for dev
-  console.log(err.stack.red);
+  console.log(err.name);
 
   //Mongoose bad ObjectId
   if (err.name === 'CastError') {
     const message = `Resource with id of ${err.value} not found`;
     error = new ErrorResponse(message, 404);
+  }
+
+  //Mongoose duplicate key
+  if (err.code === 11000) {
+    const message = 'Bootcamp already exists with same name';
+    error = new ErrorResponse(message, 400);
+  }
+
+  //Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res
