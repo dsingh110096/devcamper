2b66893051b0b9b0c0f78481dbec2eafd7a6e409
Dheerenda Singh <dsingh110096@gmail.com>
2020-07-24 16:45:49 +0530

    variable name correction

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
-const mogoose = require('mongoose');
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const conn = await mogoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
