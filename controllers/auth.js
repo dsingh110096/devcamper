63752cd72ac1cfee787b679b164ee062e46eb64a
Dheerenda Singh <dsingh110096@gmail.com>
2020-07-28 15:47:45 +0530

    user register functionality added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,12 +3,22 @@ const ErrorResponse = require('../utils/errorResponse');
 //Middleware file
 const asyncHandler = require('../middleware/async');
 
-//Course Model file
-const Course = require('../models/Course');
+//User Model file
+const User = require('../models/User');
 
 //@desc     Register User
 //route     GET /api/v1/auth/register
 //access    public
 exports.register = asyncHandler(async (req, res, next) => {
-  res.status(200).json({ success: true });
+  const { name, email, password, role } = req.body;
+
+  //Create User
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+  });
+
+  res.status(200).json({ success: true, data: user });
 });
