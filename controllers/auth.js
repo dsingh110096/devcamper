0b641d38373ca1003b03441cdb64366dc6cb4f68
Dheerenda Singh <dsingh110096@gmail.com>
2020-07-29 15:45:29 +0530

    forgot password route and get hashed token for resetPasswordToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,6 +49,41 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
+//@desc     GET current logged in user
+//route     POST /api/v1/auth/me
+//access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+//@desc     Forgot password
+//route     POST /api/v1/auth/forgotpassword
+//access    Public
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  //Check if user exists
+  if (!user) {
+    return next(
+      new ErrorResponse(`There is no user with email ${req.body.email} exists.`)
+    );
+  }
+
+  //Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
 //Get token from model, create cookie and send response
 const sendTokenResponse = (user, statusCode, res) => {
   //Create token
@@ -71,14 +106,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     .cookie('token', token, options)
     .json({ success: true, token });
 };
-
-//@desc     GET current logged in user
-//route     POST /api/v1/auth/me
-//access    Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,16 @@ const { protect } = require('../middleware/auth');
 
 //Controller files
 //Auth controller
-const { register, login, getMe } = require('../controllers/auth');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+} = require('../controllers/auth');
 
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/me').get(protect, getMe);
+router.route('/forgotpassword').post(forgotPassword);
 
 module.exports = router;
