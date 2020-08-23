8203285dc43039f5ffe2c81a64b742db1c5fe757
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-23 15:22:32 +0530

    loaduser functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,28 @@
 import './css/bootstrap.css';
 import './css/style.css';
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 //Redux Store files
 import { Provider } from 'react-redux';
 import store from './store';
-import Alert from './components/layout/Alert';
+
+//Check if token in localStorage and set it to the header
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
     <Provider store={store}>
       <Router>
