9b60627de6bb81004326ab3d0385bfd894484530
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-25 17:12:46 +0530

    showing account settings for user

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -30,10 +30,31 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   );
   const authLinks = (
     <ul className='navbar-nav ml-auto'>
-      <li className='nav-item'>
-        <Link className='nav-link' to='/' onClick={logout}>
-          <i className='fas fa-sign-in-alt'></i> Logout
-        </Link>
+      <li className='nav-item dropdown'>
+        <a
+          className='nav-link dropdown-toggle'
+          href='!#'
+          id='navbarDropdown'
+          role='button'
+          data-toggle='dropdown'
+        >
+          <i className='fas fa-user'></i> Account
+        </a>
+        <div className='dropdown-menu'>
+          <Link className='dropdown-item' to='/manage-bootcamp'>
+            Manage Bootcamp
+          </Link>
+          <Link className='dropdown-item' to='/manage-reviews'>
+            Manage Reviews
+          </Link>
+          <Link className='dropdown-item' to='/manage-account'>
+            Manage Account
+          </Link>
+          <div className='dropdown-divider'></div>
+          <Link className='dropdown-item' to='/' onClick={logout}>
+            <i className='fas fa-sign-out-alt'></i> Logout
+          </Link>
+        </div>
       </li>
       <li className='nav-item'>
         <Link className='nav-link' to='/bootcamps'>
