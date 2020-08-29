372819c266ca2d5a86ff9c1ff1b3c7cc21a5028b
Dheerenda Singh <dsingh110096@gmail.com>
2020-08-29 18:23:58 +0530

    showing guest link first

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,30 +4,6 @@ import { connect } from 'react-redux';
 import { logout } from '../../actions/auth';
 
 const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
-  const guestLinks = (
-    <ul className='navbar-nav ml-auto'>
-      <li className='nav-item'>
-        <Link className='nav-link' to='/login'>
-          <i className='fas fa-sign-in-alt'></i> Login
-        </Link>
-      </li>
-      <li className='nav-item'>
-        <Link className='nav-link' to='/register'>
-          <i className='fas fa-user-plus'></i> Register
-        </Link>
-      </li>
-      <li className='nav-item d-none d-sm-block'>
-        <Link className='nav-link' to='!#'>
-          |
-        </Link>
-      </li>
-      <li className='nav-item'>
-        <Link className='nav-link' to='/bootcamps'>
-          Browse Bootcamps
-        </Link>
-      </li>
-    </ul>
-  );
   const authLinks = (
     <ul className='navbar-nav ml-auto'>
       <li className='nav-item dropdown'>
@@ -63,6 +39,31 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
       </li>
     </ul>
   );
+  const guestLinks = (
+    <ul className='navbar-nav ml-auto'>
+      <li className='nav-item'>
+        <Link className='nav-link' to='/login'>
+          <i className='fas fa-sign-in-alt'></i> Login
+        </Link>
+      </li>
+      <li className='nav-item'>
+        <Link className='nav-link' to='/register'>
+          <i className='fas fa-user-plus'></i> Register
+        </Link>
+      </li>
+      <li className='nav-item d-none d-sm-block'>
+        <Link className='nav-link' to='!#'>
+          |
+        </Link>
+      </li>
+      <li className='nav-item'>
+        <Link className='nav-link' to='/bootcamps'>
+          Browse Bootcamps
+        </Link>
+      </li>
+    </ul>
+  );
+
   return (
     <nav className='navbar navbar-expand-md navbar-dark bg-primary sticky-top'>
       <div className='container'>
