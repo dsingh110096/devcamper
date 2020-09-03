e9e76ed88610ab9d2ed5681c3107d66819c04144
Dheerenda Singh <dsingh110096@gmail.com>
2020-09-03 09:20:46 +0530

    photo upload type case removed

--- client/src/reducers/bootcamp.js
+++ client/src/reducers/bootcamp.js
@@ -4,7 +4,6 @@ import {
   GET_SINGLE_BOOTCAMP,
   GET_BOOTCAMP_IN_RADIUS,
   FITERED_BOOTCAMPS,
-  PAGINATED_BOOTCAMPS,
   CLEAR_BOOTCAMPS,
 } from '../actions/types';
 
@@ -20,7 +19,6 @@ export default function (state = initialState, action) {
     case GET_BOOTCAMP_IN_RADIUS:
     case FITERED_BOOTCAMPS:
     case GET_ALL_BOOTCAMPS:
-    case PAGINATED_BOOTCAMPS:
       return {
         ...state,
         bootcamps: payload,
