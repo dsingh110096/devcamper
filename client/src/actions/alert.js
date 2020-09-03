23ada939120fef5ba3f04243d1ef65d2316ec7b5
Dheerenda Singh <dsingh110096@gmail.com>
2020-09-03 09:18:58 +0530

    scrollToTop added on alert

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
+import scrollToTop from '../utils/scrollToTop';
 
 //Using dispatch we can dispatch more than one action type at a time
 export const setAlert = (msg, alertType) => (dispatch) => {
   const id = uuidv4();
   //Dispatching data to the alert reducer
+  scrollToTop();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id },
